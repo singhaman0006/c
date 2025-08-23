59d4211d479c0a2c29b45b38f9d7136516b8c7ca
singhaman0006 <amansingh12446@gmail.com>
2025-08-23 22:33:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contact Form Example
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("✅ Thank you! Your message has been submitted.");
+  this.reset();
+});
